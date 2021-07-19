38f997a7c1834ffbfb0acb332692794946505a3e
Shruti Jawale <66550110+shrutz2007@users.noreply.github.com>
2021-07-19 18:20:20 +0530

    Add files via upload

---
+++ Binary to decimal.cpp
@@ -0,0 +1,21 @@
+//Binary to Decimal
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int btd(int n){
+    int ans=0,x=1;
+    while(n>0){
+        int y=n%10;
+        ans+=x*y;
+        x*=2;
+        n/=10;
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<btd(n)<<endl;
+}
